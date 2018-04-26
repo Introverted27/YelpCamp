1fc13ebedd1ac40b5a11fbca510c6ac670422f2b
Kavita Aherwar <37935155+Introverted27@users.noreply.github.com>
2018-04-27 00:20:10 +0530

    add process.env.PORT

--- app.js
+++ app.js
@@ -57,6 +57,6 @@ app.use("/", indexRoutes);
 app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/comments", commentRoutes);
 
-app.listen(process.env.port || 2700,  function(){
+app.listen(process.env.PORT || 2700,  function(){
   console.log("Server Started");
 });
